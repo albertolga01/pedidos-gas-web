671b89c1df4ef316bcaf83896359d03ca2480ee8
albertolga01 <alberto.lga01@gmail.com>
2023-11-11 12:43:25 -0700

    20231111

--- src/PasarelaPago.js
+++ src/PasarelaPago.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import './App.css'; 
+import './OpenPay.css'; 
 import Modal from 'react-modal';
 import { ThreeDots } from  'react-loader-spinner' 
 import {Navbar} from './component/Navbar';  
@@ -44,6 +45,8 @@ function PasarelaPago(props){
     const[Colonia, setColonia] = useState();
     const[Cantidad, setCantidad] = useState();
     const[CodigoPostal, setCodigoPostal] = useState(); 
+    const[tokenId, setTokenId] = useState(); 
+    const[hiddenId, setHiddenId] = useState(); 
     const [fechaHoy, setFechaHoy] = useState("null");
 
     const [modalIsOpenLoad, setIsOpenLoad] = React.useState(false);
@@ -54,8 +57,7 @@ function PasarelaPago(props){
     const[MensajeError, setMensajeError] = useState(); 
  
 	useEffect(() => {
-        obtenerConsumidor();
-        
+        loadOpenPay();
 	},[])
   
 
@@ -88,68 +90,148 @@ function PasarelaPago(props){
         props.unmount("MenuPrincipal");   
     }
 
-    async function obtenerConsumidor(){    
+    function loadOpenPay(){
+        window.OpenPay.setId('mq63xhurcngj88wral19');
+        window.OpenPay.setApiKey('pk_c2209ef4c4f74a6dabc718cf819b0619');
+        
+        var deviceSessionId = window.OpenPay.deviceData.setup("payment-form", "deviceIdHiddenFieldName");
+        setHiddenId(deviceSessionId);
+        window.OpenPay.setSandboxMode(true);
+    }
+
+    async function openPay(token_id){    
 		let fd = new FormData()   
-		fd.append("id", "obtenerConsumidor")  
-		fd.append("folioconsumidor", props.numero_consumidor) 
-		//setisLoggedIn(false);
+		fd.append("id", "openPay")  
+		fd.append("name", "Alberto") 
+		fd.append("last_name", "Lizarraga") 
+		fd.append("phone_number", "6693259307") 
+		fd.append("email", "desarrollo@grupopetromar.com") 
+		fd.append("token_id", token_id) 
+		fd.append("amount", "1.00") 
+		fd.append("description", "Compra Gas LP") 
+		fd.append("deviceIdHiddenFieldName", hiddenId) 
         openModalLoad();
 		const res = await axios.post(process.env.REACT_APP_API_URL, fd);
         closeModalLoad();
-		console.log(res.data);  
-        setComentarios(res.data[0].comentario);
-        setCalleNumero(res.data[0].calle_numero);
-        setColonia(res.data[0].colonia); 
-		setCodigoPostal(res.data[0].codigo_postal); 
+		console.log(res.data);   
 		//console.log(res.data); 
 	}
+
+    var success_callbak = function(response) {
+        var token_id = response.data.id;
+        setTokenId(token_id);
+        alert(token_id);
+        //$('#token_id').val(token_id);
+        openPay(token_id);
+    };
+
+    function Submit(e){
+        alert("submit");
+        e.preventDefault();
+        document.getElementById("pay-button").setAttribute("disabled", true);
+        //$("#pay-button").prop( "disabled", true);
+       
+        window.OpenPay.token.extractFormAndCreate('payment-form', success_callbak, error_callbak); 
+
+    }
+
+    var error_callbak = function(response) {
+    var desc = response.data.description != undefined ?
+    response.data.description : response.message;
+    alert("ERROR [" + response.status + "] " + desc);
+    //$("#pay-button").prop("disabled", false);
+    document.getElementById("pay-button").setAttribute("disabled", false);
+    };
 
      
 
     return(
-        <div   style={{margin: 'auto', width:'80%' , height: '100vh', backgroundImage: Backgroundgas}} align="center"> 
+        <div   style={{margin: 'auto', width:'100%' , height: '100vh', backgroundImage: Backgroundgas}} align="center"> 
 
          
             
-                <div style={{ display: 'flex', flexDirection: 'column', width: '100%', }} align="center">
-
-                <Navbar titulo="Pagar" />
-                <br></br>
-                <br></br>
-                      <div class="pymnts">
-                          <form action="#" method="POST" id="payment-form">
+                <div style={{ display: 'flex', flexDirection: 'column', maxWidth: '100%', }} align="center">
+                    {tokenId}
+                      <div  style={{alignSelf:'center'}}>
+                          <form action="#" method="POST" id="payment-form" class="pymnts">
                               <input type="hidden" name="token_id" id="token_id"/>
                               <div class="pymnt-itm card active">
-                                  <h2>Tarjeta de crédito o débito</h2>
-                                  <div class="pymnt-cntnt">
+                                    
+                                  <div class="pymnt-cntnt" style={{width:'344px', height:'950px'}}>
+                                  <div class="card-expl">
+                                        <h2 style={{width:'344px'}}>Tarjeta de crédito o débito</h2>
+                                    </div>
                                       <div class="card-expl">
                                           <div class="credit"><h4>Tarjetas de crédito</h4></div>
+                                      </div>
+                                      <div class="card-expl">
                                           <div class="debit"><h4>Tarjetas de débito</h4></div>
                                       </div>
+
                                       <div class="sctn-row">
                                           <div class="sctn-col l">
                                               <label>Nombre del titular</label><input type="text" placeholder="Como aparece en la tarjeta" autocomplete="off" data-openpay-card="holder_name"/>
+                                            </div>
+                                       </div>
+
+                                       <div class="sctn-row">
+                                          <div class="sctn-col l">
+                                          <label>Número de tarjeta</label><input type="text" autocomplete="off" data-openpay-card="card_number"/>                                               
                                           </div>
-                                          <div class="sctn-col">
-                                              <label>Número de tarjeta</label><input type="text" autocomplete="off" data-openpay-card="card_number"/></div>
+                                        </div>
+
+
+                                        <div class="sctn-row">
+                                          <div class="sctn-col l">
+                                          <label>Fecha de expiración</label>
+                                                        <div class="sctn-col half l"><input type="text" placeholder="Mes" data-openpay-card="expiration_month"/></div>
+                                                        <div class="sctn-col half l"><input type="text" placeholder="Año" data-openpay-card="expiration_year"/></div>
                                           </div>
-                                          <div class="sctn-row">
-                                              <div class="sctn-col l">
-                                                  <label>Fecha de expiración</label>
-                                                  <div class="sctn-col half l"><input type="text" placeholder="Mes" data-openpay-card="expiration_month"/></div>
-                                                  <div class="sctn-col half l"><input type="text" placeholder="Año" data-openpay-card="expiration_year"/></div>
-                                              </div>
-                                              <div class="sctn-col cvv"><label>Código de seguridad</label>
-                                                  <div class="sctn-col half l"><input type="text" placeholder="3 dígitos" autocomplete="off" data-openpay-card="cvv2"/></div>
-                                              </div>
+                                        </div>
+
+
+                                        <div class="sctn-row">
+                                          <div class="sctn-col l">
+                                          <div class="sctn-col cvv"><label>Código de seguridad</label>
+                                                        <div class="sctn-col half l"><input type="text" placeholder="3 dígitos" autocomplete="off" data-openpay-card="cvv2"/></div>
+                                                    </div>
+
                                           </div>
-                                          <div class="openpay"><div class="logo">Transacciones realizadas vía:</div>
-                                          <div class="shield">Tus pagos se realizan de forma segura con encriptación de 256 bits</div>
-                                      </div>
-                                      <div class="sctn-row">
-                                              <a class="button rght" id="pay-button">Pagar</a>
-                                      </div>
-                                  </div>
+                                        </div>
+
+                                        
+
+                                        <div class="sctn-row">
+                                            <div class="sctn-col l">
+                                                <div class="sctn-row">
+                                                  <div class="openpay">
+                                                    <div class="logo">Transacciones realizadas vía:</div>
+                                                 </div>
+                                            </div>
+                                        </div>
+                                        </div>
+
+                                        <div class="sctn-row">
+                                            <div class="sctn-col l">
+                                                <div class="sctn-row">
+                                                     <div class="shield">Tus pagos se realizan de forma segura con encriptación de 256 bits</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                   
+                                        <div class="sctn-row">
+                                            <div class="sctn-col l">
+                                                <div class="sctn-row">
+                                                            <a class="button rght" id="pay-button" onClick={(e) => Submit(e)}>Pagar</a>
+                                                </div>
+                                            </div>
+                                        </div>
+ 
+
+                                     
+                                           
+                                       
+                                    </div>
                               </div>
                           </form>
                       </div>
@@ -160,7 +242,7 @@ function PasarelaPago(props){
                     <button className="buttonVerde" style={{width:'100%', fontWeight: 'bold'}} onClick={() => { Seleccionar();}}>Regresar</button>
                     </div>
                     <div style={{width:'50%'}} align="center"> 
-                        <button type='submit' className='button' style={{ fontWeight: 'bold', width:'100%'}}>Confirmar Abono</button>
+                        <button   className='button' style={{ fontWeight: 'bold', width:'100%'}} onClick={() => Submit()}>Confirmar Abono</button>
                         </div>
                     </div>                    
                     <br></br>
